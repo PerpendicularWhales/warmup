2b55f250a66297ce4ccf37705a32a354de004d62
adiospl <adios.pl@gmail.com>
2016-09-26 22:57:36 +0200

    Introduces WorldConfiguration

--- core/src/com/perpendicularwhales/warmup/Game.java
+++ core/src/com/perpendicularwhales/warmup/Game.java
@@ -1,33 +1,38 @@
 package com.perpendicularwhales.warmup;
 
+import com.artemis.World;
+import com.artemis.WorldConfiguration;
+import com.artemis.WorldConfigurationBuilder;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private World world;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		WorldConfiguration configuration = new WorldConfigurationBuilder()
+				.build();
+
+		OrthographicCamera camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		configuration.register(camera);
+
+		world = new World(configuration);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		world.setDelta(Gdx.graphics.getDeltaTime());
+		world.process();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
